e2b43c0666d078dfe181814384b749281313839e
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-08-29 20:03:00 +0530

    added kmeans clustering code and hoof_to_gsom function

--- gsom/applications/video_highlights/video_hoof_gsom.py
+++ gsom/applications/video_highlights/video_hoof_gsom.py
@@ -13,23 +13,9 @@ from util import display as Display_Utils
 
 from params import params as Params
 from core4 import core_controller as Core
+from util import kmeans_cluster_gsom as KMeans_Cluster
 
-# GSOM config
-SF = 0.9
-forget_threshold = 60  # To include forgetting, threshold should be < learning iterations.
-temporal_contexts = 1  # If stationary data - keep this at 1
-learning_itr = 80
-smoothing_irt = 40
-plot_for_itr = 4  # Unused parameter - just for visualization. Keep this as it is.
-
-# File Config
-dataset = 'video'
-data_filename = "data/1.mp4".replace('\\', '/')
-
-experiment_id = 'Exp-' + datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
-output_save_location = join('output/', experiment_id)
-
-def generate_output_config(dataset, SF, forget_threshold):
+def generate_output_config(dataset, SF, forget_threshold, output_save_location):
 
     # Output data config
     output_save_filename = '{}_data_'.format(dataset)
@@ -47,39 +33,76 @@ def generate_output_config(dataset, SF, forget_threshold):
 
     return output_loc, output_loc_images
 
+def hoof_to_gsom(SF, learning_itr, smoothing_irt, plot_for_itr, forget_threshold, data_filename, dataset, output_save_location):
+    # Init GSOM Parameters
+    gsom_params = Params.GSOMParameters(SF, learning_itr, smoothing_irt, distance=Params.DistanceFunction.EUCLIDEAN,
+                                        temporal_context_count=temporal_contexts, forget_itr_count=forget_threshold)
+
+    generalise_params = Params.GeneraliseParameters(gsom_params)
+
+    # Process the input files
+    input_database, labels = Parser.InputParser.get_video_hoof_feature_vector(data_filename)
+    # input_data_features_list, labels = get_video_feature_vector()
+    output_loc, output_loc_images = generate_output_config(dataset, SF, forget_threshold, output_save_location)
+
+    # Setup the age threshold based on the input vector length
+    generalise_params.setup_age_threshold(input_database[0].shape[0])
+
+    # Process the clustering algorithm algorithm
+    controller = Core.Controller(generalise_params)
+    controller_start = time.time()
+    result_dict = controller.run(input_vector_db=input_database,  # return the list/map from here
+                                 plot_for_itr=plot_for_itr,
+                                 output_loc=output_loc_images
+                                 )
+    print('Algorithms completed in', round(time.time() - controller_start, 2), '(s)')
+    saved_name = Utils.Utilities.save_object(result_dict, join(output_loc, 'gsom_nodemap_SF-{}'.format(SF)))
+
+    gsom_nodemap = result_dict[0]['gsom']
+
+    # Display
+    display = Display_Utils.Display(result_dict[0]['gsom'], None)
+    # display.setup_labels_for_gsom_nodemap(labels, 2, 'Latent Space of {} : SF={}'.format(dataset, SF),
+    #                                       join(output_loc, 'latent_space_' + str(SF) + '_hitvalues'))
+    display.setup_labels_for_gsom_nodemap(labels, 3, 'Latent Space of {} : SF={}'.format(dataset, SF),
+                                          join(output_loc, 'latent_space_' + str(SF) + '_labels'))
+
+    print('Completed.')
+    return gsom_nodemap
+
 if __name__ == '__main__':
 
-        # Init GSOM Parameters
-        gsom_params = Params.GSOMParameters(SF, learning_itr, smoothing_irt, distance=Params.DistanceFunction.EUCLIDEAN,
-                                            temporal_context_count=temporal_contexts, forget_itr_count=forget_threshold)
+    # GSOM config
+    SF = 0.9
+    forget_threshold = 60  # To include forgetting, threshold should be < learning iterations.
+    temporal_contexts = 1  # If stationary data - keep this at 1
+    learning_itr = 80
+    smoothing_irt = 40
+    plot_for_itr = 4  # Unused parameter - just for visualization. Keep this as it is.
 
-        generalise_params = Params.GeneraliseParameters(gsom_params)
+    # File Config
+    dataset = 'video'
+    data_filename = "data/1.mp4".replace('\\', '/')
 
-        # Process the input files
-        input_database, labels = Parser.InputParser.get_video_hoof_feature_vector(data_filename)
-        # input_data_features_list, labels = get_video_feature_vector()
-        output_loc, output_loc_images = generate_output_config(dataset, SF, forget_threshold)
+    experiment_id = 'Exp-' + datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+    output_save_location = join('output/', experiment_id)
 
-        # Setup the age threshold based on the input vector length
-        generalise_params.setup_age_threshold(input_database[0].shape[0])
+    gsom_nodemap = hoof_to_gsom(
+        SF,
+        learning_itr,
+        smoothing_irt,
+        plot_for_itr,
+        forget_threshold,
+        data_filename,
+        dataset,
+        output_save_location
+    )
 
-        # Process the clustering algorithm algorithm
-        controller = Core.Controller(generalise_params)
-        controller_start = time.time()
-        result_dict = controller.run(input_vector_db=input_database, #return the list/map from here
-                                     plot_for_itr= plot_for_itr,
-                                     output_loc=output_loc_images
-                                     )
-        print('Algorithms completed in', round(time.time() - controller_start, 2), '(s)')
-        saved_name = Utils.Utilities.save_object(result_dict, join(output_loc, 'gsom_nodemap_SF-{}'.format(SF)))
-
-        gsom_nodemap = result_dict[0]['gsom']
-
-        # Display
-        display = Display_Utils.Display(result_dict[0]['gsom'], None)
-        # display.setup_labels_for_gsom_nodemap(labels, 2, 'Latent Space of {} : SF={}'.format(dataset, SF),
-        #                                       join(output_loc, 'latent_space_' + str(SF) + '_hitvalues'))
-        display.setup_labels_for_gsom_nodemap(labels, 3, 'Latent Space of {} : SF={}'.format(dataset, SF),
-                                              join(output_loc, 'latent_space_' + str(SF) + '_labels'))
-
-        print('Completed.')
+    kmeans_som = KMeans_Cluster.KMeansSOM()
+    gsom_list, centroids, labels = kmeans_som.cluster_GSOM(gsom_nodemap, 4)
+    print("gsom list")
+    print(gsom_list)
+    print("centroids")
+    print(centroids)
+    print("labels")
+    print(labels)
