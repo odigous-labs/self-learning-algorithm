4a249aa643ea350e1eafe20252440a116370a51a
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2019-09-10 21:20:02 +0530

    added_static_features

---
+++ gsom/applications/video_highlights/static_features/CreateFeaturesArray.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+import sys
+sys.path.append('../../')
+from gsom.feature_extracter import VGG
+
+
+def getFeatureArray(frames_folder_path):
+    features_list = []
+    labels_list = []
+    list_of_frames = os.listdir(frames_folder_path);
+    feature_extracter = VGG.VGG()
+    for frame in list_of_frames:
+        feature = feature_extracter.getFeatureVector(frames_folder_path+"/"+frame)
+        features_list.append(feature)
+        labels_list.append(frame)
+        print (frame+" processed...")
+    features_array = np.asarray(features_list)
+    features_matrix = np.asmatrix(features_array)
+    return features_matrix,labels_list
+
+def run():
+    getFeatureArray("./../generated_frames")
+
+if __name__ == "__main__":
+    run()
---
+++ gsom/applications/video_highlights/static_features/data_parser.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('../../')
+from applications.video_highlights.static_features import CreateFeaturesArray
+
+
+class InputParser:
+
+    @staticmethod
+    def parse_input_frames(path_to_frames = "./../generated_frames"):
+        feature_matrix,labels = CreateFeaturesArray.getFeatureArray(path_to_frames)
+        input_database = {
+            0: feature_matrix
+        }
+        return input_database, labels
